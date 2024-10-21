efd793a6dcfb045a11b1951494c1fc4ade07797b
woollim <137277221+woollim23@users.noreply.github.com>
2024-10-21 14:03:57 +0900

    update project

---
+++ Unity_SteadyPractice_Week2/Assets/Scripts/InputRebinder.cs
@@ -0,0 +1,53 @@
+using System.Buffers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputRebinder : MonoBehaviour
+{
+    public InputActionAsset actionAsset;
+    private InputAction spaceAction;
+    private InputAction escapeAction;
+
+    void Start()
+    {
+        // (완료) [구현사항 1] actionAsset에서 Space 액션을 찾고 활성화합니다.
+        spaceAction = new InputAction(binding: "<Keyboard>/space");
+    }
+
+    // (완료) [구현사항 2] ContextMenu 어트리뷰트를 활용해서 인스펙터창에서 적용할 수 있도록 함
+
+    void OnEnable()
+    {
+        // Esc 키에 대한 액션 생성
+        escapeAction = new InputAction(binding: "<Keyboard>/escape");
+
+        // Esc 키 입력 이벤트 등록
+        escapeAction.performed += OnEscapePressed;
+
+        // 액션 활성화
+        escapeAction.Enable();
+    }
+
+    private void OnEscapePressed(InputAction.CallbackContext context)
+    {
+        // Esc 키 눌렀을 때 실행
+        RebindSpaceToEscape();
+    }
+
+    public void RebindSpaceToEscape()
+    {
+        if (spaceAction == null)
+            return;
+        actionAsset.FindAction("Sapce").ApplyBindingOverride("<Keyboard>/escape");
+
+        Debug.Log("Done!");
+    }
+
+    void OnDestroy()
+    {
+        // 액션을 비활성화합니다.
+        spaceAction?.Disable();
+    }
+}
---
+++ Unity_SteadyPractice_Week2/README.md
@@ -0,0 +1,11 @@
+스파르타코딩 꾸준실습 유니티 기록용 깃허브 입니다 (개발도구 : Unity, C#)
+
+## ■ **Week2_Challenge**
+### ○ 개발기간 : 2024-10-21 ~
+### ○ 구현 기능
+- Q1.  Input Rebinding
+   - [구현사항 0] (완료)Input System을 import 하고, Input Action Challenge를 구성하세요.
+   - [구현사항 1] (완료)InputRebinder 클래스의 actionAsset에서 Space 액션을 찾고 활성화합니다.
+   - [구현사항 2] (완료)InputRebinder 클래스의 RebindSpaceToEscape 메소드를 ContextMenu 어트리뷰트를 활용해서 인스펙터창에서 우클릭으로 실행할 수 있도록 합니다.
+   - [구현사항 3] InputRebinder 클래스의 RebindSpaceToEscape 함수는 기존 Spacebar 키를 입력하던 액션을 Escape키를 입력했을 때 발생하도록 합니다. 이와 관련된 레퍼런스 문서를 읽고, 완성하시오.
+<br>
